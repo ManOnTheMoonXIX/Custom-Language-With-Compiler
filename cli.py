7d0b0acedd303395f42cfba78e2b22dd41bd9b60
Petey17 <114273296+Petey17@users.noreply.github.com>
2025-04-05 22:24:38 -0500

    Update cli.py
    Added Group Member Names

--- cli.py
+++ cli.py
@@ -1,3 +1,10 @@
+# Group Members: 
+# Justin Alder 2007273
+# Daryn Brown 2002414
+# Marvis Haughton 1802529
+# Peta Gaye Mundle 1403906 
+# Cassandra Powell 2005742
+
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
 from parser import parser
