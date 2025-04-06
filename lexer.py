4763f53545d9a9d22c0cfb4e69163770c8b3d0c9
Petey17 <114273296+Petey17@users.noreply.github.com>
2025-04-05 22:15:02 -0500

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,3 +1,12 @@
+/*
+Group Members: 
+Justin Alder 2007273
+Daryn Brown 2002414
+Marvis Haughton 1802529
+Peta Gaye Mundle 1403906 
+Cassandra Powell 2005742
+*/
+
 import ply.lex as lex
 
 # Reserved Words (excluding types like CONCERT, MOVIE)
